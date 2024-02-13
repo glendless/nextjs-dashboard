16bbb4789d8402de1e73cf5236bd96d5396504e7
glendless <glenddelonge@gmail.com>
2024-02-13 15:02:08 +0700

    delete placeholder at searchbox

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({
             </div>
 
             <div className="mt-4 flex items-center justify-between gap-2 ">
-                <Search placeholder="Search invoices ..." />
+                <Search />
                 <CreateInvoice />
             </div>
 
@@ -35,7 +35,7 @@ export default async function Page({
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages}/>
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     )
